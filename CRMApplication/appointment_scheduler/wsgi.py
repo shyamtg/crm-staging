4eb6231fdfa9fcecd9c05294ce3daefa77023e8c
shyam_tg <shyamsundar.tg@gmail.com>
2019-11-21 18:25:28 +0530

    req

--- CRMApplication/appointment_scheduler/wsgi.py
+++ CRMApplication/appointment_scheduler/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appointment_scheduler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRMApplication/appointment_scheduler.settings')
 
 application = get_wsgi_application()
